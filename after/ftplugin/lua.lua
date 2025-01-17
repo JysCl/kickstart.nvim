c431d482e3c053d85c2a2858ecd17df990bcbf83
JysCl <theshartnation@proton.me>
2025-01-17 14:59:45 -0500

    Changed architecture for plugins

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,8 @@
+-- This is were you would do settings for lua files
+-- ex: 
+-- This is to use set the way vimscript did back then, opt_LOCAL
+-- local set = vim.opt_local
+
+-- set.shiftwidth = 2 
+-- set.number = true
+-- set.relativenumber = true
